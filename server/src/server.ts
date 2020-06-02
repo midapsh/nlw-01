cd3f4758b0d1d8bde7630fa75086f5489ec343aa
midapsh <34818053+midapsh@users.noreply.github.com>
2020-06-01 23:32:19 -0300

    Add response

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,16 @@ import express from "express";
 // Inicializando arquivo de inicializacao do typescript
 // Video 1: 57:53
 // npx tsc --init
-
+// Inicializar aplicacao
+// npx ts-node src/server.ts
 const app = express();
 
 app.get(
   "/users",
-  () => {
+  (request, response) => {
     console.log("listagem de usuario");
+    const msg = "Hello Oi";
+    response.json({ msg });
   },
 );
 
