6abdb48f5730740b85b8296cb39c98c0ea1655cf
midapsh <34818053+midapsh@users.noreply.github.com>
2020-06-02 23:25:05 -0300

    New return; Add ignore flag to 'npm run dev'

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,19 @@
 import express from "express";
+import knex from "./database/connection";
 
 const routes = express.Router();
 
-routes.get("/", (request, response) => {
-  const msg = "Hello";
-  return response.json({ msg });
+routes.get("/items", async (request, response) => {
+  const items = await knex("items").select("*");
+
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:1234/uploads/${item.image}`,
+    };
+  });
+
+  return response.json(serializedItems);
 });
 
 export default routes;
