2144c7fc646634d14f1a93d7a615b46a2940e828
midapsh <34818053+midapsh@users.noreply.github.com>
2020-06-06 23:39:11 -0300

    Remove Header example

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import './App.css';
 
-import Header from "./Header";
-
 function App() {
   return (
-    <Header 
-      title="Hello World"
-    />
+    <h1>Hello World</h1>
   );
 }
 
