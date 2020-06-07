022c4102ee06103d3036da7e93557f305dc1f12e
midapsh <34818053+midapsh@users.noreply.github.com>
2020-06-07 20:13:58 -0300

    Add validation

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { celebrate, Joi } from "celebrate";
 
 import multer from "multer";
 import multerConfig from "./config/multer";
@@ -16,6 +17,22 @@ routes.get("/items", itemsController.index);
 routes.get("/points", pointsController.index);
 routes.get("/points/:id", pointsController.show);
 
-routes.post("/points", upload.single("image"), pointsController.create);
+routes.post(
+  "/points",
+  upload.single("image"),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string(),
+      email: Joi.string().required(),
+      whatsapp: Joi.string().required().email(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required(),
+    })
+  }),
+  pointsController.create
+);
 
 export default routes;
