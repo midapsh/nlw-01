b465d150520e59710472e15febb14f3aeb7cc9d4
midapsh <34818053+midapsh@users.noreply.github.com>
2020-06-07 20:14:10 -0300

    Return errors with celebrate

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import cors from "cors";
 import routes from "./routes";
+import { errors } from "celebrate";
 // Inicializando arquivo de inicializacao do typescript
 // Video 1: 57:53
 // npx tsc --init
@@ -16,5 +17,7 @@ app.use(express.json());
 app.use(routes);
 
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
+
+app.use(errors());
 
 app.listen(1234);
