aa5e17943b469173113e9e43afa18ed3e576980d
midapsh <34818053+midapsh@users.noreply.github.com>
2020-06-07 00:39:07 -0300

    Change extension

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { FiLogIn } from "react-icons/fi";
+import { Link } from "react-router-dom";
+
 import './styles.css';
+
 import logo from "../../assets/logo.svg";
 
 const Home = () => {
@@ -14,12 +17,12 @@ const Home = () => {
           <h1>Seu marketplace de coleta de resíduos.</h1>
           <p>Ajudamos pessoas a encontrarem pontos de oleta de forma eficiente.</p>
 
-          <a href="/cadastro">
+          <Link to="/create-point">
             <span>
               <FiLogIn />
             </span>
             <strong>Cadastre um ponto de coleta</strong>
-          </a>
+          </Link>
         </main>
       </div>
     </div>
