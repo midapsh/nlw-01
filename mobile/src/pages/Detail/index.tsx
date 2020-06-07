7f389c140d67678e46c379529af774e32ba218b8
midapsh <34818053+midapsh@users.noreply.github.com>
2020-06-07 16:13:07 -0300

    Add Detail page

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,9 +1,67 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { Feather as Icon, FontAwesome } from "@expo/vector-icons";
 import Constants from "expo-constants";
+import { useNavigation } from "@react-navigation/native";
+import { View, StyleSheet, Image, Text, SafeAreaView } from "react-native";
+import { RectButton, TouchableOpacity } from "react-native-gesture-handler";
 
 const Detail = () => {
-  return <View />;
+  const navigation = useNavigation();
+
+  function handleNavigateBack() {
+    navigation.goBack();
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack}>
+          <Icon
+            name="arrow-left"
+            size={20}
+            color="#34CB79"
+          />
+        </TouchableOpacity>
+        <Image
+          style={styles.pointImage}
+          source={{
+            uri: "https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+          }}
+        />
+        <Text style={styles.pointName}>Mercadão</Text>
+        <Text style={styles.pointItems}>Lâmpadas, Óleo de Cozinha</Text>
+        <View style={styles.address}>
+          <Text style={styles.addressTitle}>Endereço</Text>
+          <Text style={styles.addressContent}>Rio do Sul, SC</Text>
+        </View>
+      </View>
+      <View style={styles.footer}>
+        <RectButton
+          style={styles.button}
+          onPress={() => { }}
+        >
+          <FontAwesome
+            name="whatsapp"
+            size={20}
+            color="#FFF"
+          />
+          <Text style={styles.buttonText}>Whatsapp</Text>
+        </RectButton>
+
+        <RectButton
+          style={styles.button}
+          onPress={() => { }}
+        >
+          <Icon
+            name="mail"
+            size={20}
+            color="#FFF"
+          />
+          <Text style={styles.buttonText}>E-mail</Text>
+        </RectButton>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -57,6 +115,7 @@ const styles = StyleSheet.create({
     borderTopWidth: StyleSheet.hairlineWidth,
     borderColor: '#999',
     paddingVertical: 20,
+    paddingBottom: 8,
     paddingHorizontal: 32,
     flexDirection: 'row',
     justifyContent: 'space-between'
